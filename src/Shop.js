ac2e7d0ab1f83ec351aa95f5084b1144fb2cad0e
Bhakti Patil <bhakti.patil@aniruddhagps.com>
2024-06-30 16:47:46 +0530

    creating react new app redux app

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+    <h2>Buy the Dress in 200Rs</h2>
+      <button type="button" class="btn btn-primary">-</button>
+      Add to Cart
+      <button type="button" class="btn btn-primary">+</button>
+    </div>
+  )
+}
+
+export default Shop
